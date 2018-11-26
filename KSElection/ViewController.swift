dbb5180af9a201f9d2aff7647c62b85889300e38
Chang Sophia <you@example.com>
2018-11-26 12:51:34 +0800

    initial

--- KSElection/ViewController.swift
+++ KSElection/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+   var number = ""
+    
+    @IBOutlet weak var kmLabel: UILabel!
+    @IBOutlet weak var dogfishLabel: UILabel!
+    @IBOutlet weak var kmImageView: UIImageView!
+    @IBOutlet weak var dogfishImageView: UIImageView!
+    @IBOutlet weak var reverse: UIButton!
+    @IBAction func reverseAction(_ sender: Any) {
+ 
+         
+        UIView.animate(withDuration: 3) {
 
+            self.dogfishImageView.frame = CGRect(origin: self.dogfishImageView.frame.origin, size: CGSize(width: 360, height: self.dogfishImageView.frame.height))
+            self.kmImageView.frame = CGRect(origin: self.kmImageView.frame.origin, size: CGSize(width: 298, height: self.kmImageView.frame.height))
+            self.kmLabel.text = String(format:"892,545" )
+            self.dogfishLabel.text = String(format:"742,239")
+        }
+      }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+      
 
 
 }
-
+}
